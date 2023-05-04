c307351b4dff69d5bdbb7b0de3abdcbaf54fcafd
Gianluka Gallarday Bustamante <gianluka@Gianlukas-MacBook-Air.local>
2023-05-03 22:48:42 -0500

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SideBarMenu } from './components/SideBarMenu';
+import { SideBarMenuCard, SideBarMenuItem } from './types/types';
+import { FcAdvertising, FcDepartment, FcGlobe } from 'react-icons/fc';
+import profileImage from './bill.jpg';
 
 function App() {
+
+  const items: SideBarMenuItem[] = [
+    {
+      id: "1",
+      label: "Aviso",
+      icon: FcAdvertising,
+      url: "/",
+    },
+    {
+      id: "2",
+      label: "Departamento",
+      icon: FcDepartment,
+      url: "/",
+    },
+    {
+      id: "2",
+      label: "Globo",
+      icon: FcGlobe,
+      url: "/",
+    },
+  ];
+
+  const card: SideBarMenuCard = 
+    {
+      id: "1",
+      displayName: "Gianluka Gallarday",
+      title: "Developer",
+      photoUrl: profileImage,
+      url: "/",
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <SideBarMenu items={items} card={card}/>
     </div>
   );
 }
